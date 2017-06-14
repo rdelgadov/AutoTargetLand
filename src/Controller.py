6a4b62ee8ff1afcb8d8885eea16f778ac0673b91
rdelgadov <rdelgadovega@gmail.com>
2017-06-14 19:21:45 -0400

    Change the current visual estimation, to visual and sensor estimation using the roll and pitch angles like parameters

--- src/Controller.py
+++ src/Controller.py
@@ -25,51 +25,83 @@ class Controller:
         self.twist = Twist()
         self.empty = Empty()
         self.TARGET_SEE = False
-        self.t = 1.0/30
+        self.t = 1.0/15
         self.last_x = 0
         self.last_y = 0
+        self.last_vx = 0
+        self.last_vy = 0
+        self.last_error_roll = 0
+        self.last_error_pitch = 0
         self.time = 0
-        self.vel_x = 0
-        self.vel_y = 0
+        self.accum_error_roll = 0
+        self.accum_error_pitch = 0
+        self.kp = 0.001
+        self.ki = 0.001
+        self.kd = 0.001
         self.last_time = 0
         self.x = 0
         self.y = 0
         self.distance = 0
         self.angle = 0
+        self.newWidth = 0
+        self.newHeight = 0
     def navCallback(self,data):
-        if self.STATE == "AUTONOMOUS":
+        if self.STATE == "TEST":
+            self.STATE = "AUTONOMOUS"
+            self.x = (data.tags_xc[0]*640/1000-320) #Pixels
+            self.y = (180-data.tags_yc[0]*360/1000) #pixels
+            self.y = self.y*0.158/width
+            self.x = self.x*0.2/height
+            self.last_vx = data.vx*1.0/1000
+            self.last_vy = data.vy*1.0/1000
+            self.twist.linear.x=1
+            self.twist.linear.y=1
+            self.update()
+        elif self.STATE == "AUTONOMOUS":
             if data.tags_count==1:
-                self.last_time = self.time
                 self.last_x = self.x
                 self.last_y = self.y
-                self.time = rospy.get_rostime()
-                self.x = data.tags_xc[0]*640/1000
-                self.y = data.tags_yc[0]*360/1000
                 height = data.tags_height[0]*640/1000
                 width = data.tags_width[0]*360/1000
+                self.x = (data.tags_xc[0]*640/1000-320) #Pixels
+                self.y = (180-data.tags_yc[0]*360/1000) #pixels
+                self.y = self.y*0.158/width
+                self.x = self.x*0.2/height
                 self.distance = data.tags_distance[0]-20
-                self.angle = data.tags_orientation[0]
-                if self.TARGET_SEE:
-                    delta_x = self.x-self.last_x
-                    delta_y = self.y-self.last_y
-                    velocity_x = (20.0*delta_x/height)*self.t
-                    velocity_y = (15.3*delta_y/width)*self.t
-                #    if delta_x > 1:
-                        #rospy.loginfo("Delta x: "+str(20.0*delta_x/height))
-                        #rospy.loginfo("Velocity x: "+str((20.0*delta_x/height)*self.t))
-                #    if delta_y > 1:
-                        #rospy.loginfo("Delta y:"+str(15.3*delta_y/width))
-                        #rospy.loginfo("Velocity x: "+str((15.3*delta_y/width)*self.t))
-                    nested_aceleration_x = 2.0*((340-self.x)*20.0/height - velocity_x*1.0/30 )*self.t*self.t
-                    rospy.loginfo("Aceleration in x: "+ str(nested_aceleration_x))
-                    linear_y = math.atan((nested_aceleration_x*1.0/981))*(180.0/math.pi)/4
-                    rospy.loginfo("linear y: " + str(linear_y))
-                    nested_aceleration_y = 0
-                self.TARGET_SEE = True
-
-            elif self.TARGET_SEE:
-                pass
-            else:
+                self.newWidth = int(round(5688.2*pow(self.distance,-1.035)))*0.158/width
+                self.newHeight = int(round(13107*pow(self.distance,-1.028)))*0.2/height
+                rospy.loginfo("tag is in : %d, %d",self.x,self.y)
+                # Todo medido desde el drone.
+                delta_y = self.x-self.last_x    #Delta en m del target.
+                delta_x = self.y-self.last_y    #Delta en m del target.
+                self.error_roll = self.twist.linear.y*4.0-data.rotX
+                self.error_pitch = self.twist.linear.x*4.0-data.rotY
+                #delta_yaw = self.twist.angular.z - data.rotZ
+                rospy.loginfo("delta_x: %d delta_y: %d", delta_x, delta_y)
+                velocity_x = -delta_x*1.0/self.t #Velocidad en m/s del drone
+                velocity_y = -delta_y*1.0/self.t #Velocidad en m/s del drone
+                rospy.loginfo("velocity_x: %d, velocity_y: %d", velocity_x, velocity_y)
+                # Si hay datos de la velocidad en el drone, se toman esas velocidades
+                self.accum_error_roll = self.accum_error_roll + error_roll*self.t
+                self.accum_error_pitch = self.accum_error_pitch + error_pitch*self.t
+                derivate_error_roll = (self.error_roll - self.last_error_roll)/self.t
+                derivate_error_pitch = (self.error_pitch - self.last_error_pitch)/self.t
+                self.last_error_roll = self.error_roll
+                self.last_error_pitch = self.error_pitch
+                rospy.loginfo("errors angles accum: %d, %d", self.accum_error_roll, self.accum_error_pitch)
+                self.accum_vel_x = self.accum_vel_x + error_velocity_x #Sumatoria caso discreto
+                self.accum_vel_y = self.accum_vel_y + error_velocity_y #Sumatoria caso discreto
+                aceleration_x = 2.0*(self.x/100 - velocity_x*self.t)/(self.t*self.t)
+                aceleration_y = 2.0*(self.y/100 - velocity_y*self.t)/(self.t*self.t)
+                self.last_vx = velocity_x
+                self.last_vy = velocity_y
+                rospy.loginfo("Aceleration: x: %s, y: %s", str(aceleration_x), str(aceleration_y))
+                linear_y = math.atan((aceleration_x*1.0/9.81))*(180.0/math.pi)/4 + self.kp*error_velocity_x+self.kd*self.accum_vel_x + self.ki*(error_velocity_x-self.last_error_x)/self.t
+                linear_x = math.atan((aceleration_y*1.0/9.81))*(180.0/math.pi)/4 + self.kp*error_velocity_y+self.kd*self.accum_vel_y + self.ki*(error_velocity_y-self.last_error_y)/self.t
+                rospy.loginfo("\n x: "+str(linear_y)+" with error: " + str(error_velocity_x*self.kp) + " and accum error: " + str(self.accum_vel_x*self.ki) + "\n y: " + str(linear_x) + " with error: " + str(error_velocity_y*self.kp) + " and accum error: "+str(self.accum_vel_y*self.ki))
+                self.twist.linear.y = -linear_y/2.0
+                self.twist.linear.x = linear_x/2.0
+            else
                 self.TARGET_SEE = False
                 self.STATE = "USER_CONTROLL"
                 rospy.loginfo("I can't see the target, controll me!")
@@ -79,7 +111,7 @@ class Controller:
         if self.STATE == "LANDED":
             if data.buttons[10]:
                 takeoff_time = rospy.get_rostime()
-                #self.takeoff.publish(self.empty)
+                self.takeoff.publish(self.empty)
                 while (rospy.get_rostime().secs-takeoff_time.secs<3):
                     rospy.loginfo("takeoff, wait: "+ str(rospy.get_rostime().secs-takeoff_time.secs)+" secs and "+ str(rospy.get_rostime().nsecs-takeoff_time.nsecs) +" nsecs")
                 rospy.loginfo("Ardrone is USER_CONTROLL!")
@@ -88,11 +120,15 @@ class Controller:
         elif self.STATE == "USER_CONTROLL":
             self.twist.linear.x = 0.1*(data.buttons[13]-data.buttons[14])
             self.twist.linear.y = (data.buttons[11]-data.buttons[12])*0.1
-            self.twist.linear.z = data.axes[4]
+            self.twist.linear.z = data.axes[4]*0.1
             self.twist.angular.z = data.axes[3]*0.1
+            self.update()
             if data.buttons[0]:
                 rospy.loginfo('autonomy drone!')
                 self.STATE = "AUTONOMOUS"
+            elif data.buttons[1]:
+                rospy.loginfo('ardrone test!')
+                self.STATE = "TEST"
 
         elif self.STATE == "AUTONOMOUS" and data.buttons[0]:
             rospy.loginfo('user controller!')
@@ -101,6 +137,15 @@ class Controller:
             self.land.publish(self.empty)
             self.STATE = "LANDED"
             rospy.loginfo('landed')
+
+    def update(self):
+        if self.x>-1.0*self.newWidth/2 and self.x<1.0*self.newWidth/2 and self.y<1.0*self.newHeight/2 and self.y > -1.0*self.newHeight/2:
+            self.twist.linear.x = 0
+            self.twist.linear.y = 0
+            self.twist.linear.z = -0.1
+        if self.distance < 45 and self.x>-1.0*self.newWidth/2 and self.x<1.0*self.newWidth/2 and self.y<1.0*self.newHeight/2 and self.y > -1.0*self.newHeight/2:
+            self.land.publish(self.empty)
+        self.move.publish(self.twist)
 
 
 def main(args):
@@ -122,11 +167,6 @@ def main(args):
         print("Service did not process request: " + str(exc))
     except rospy.exceptions.ROSException as exc:
         print("The camera change is not working, do it manually")
-    r=rospy.Rate(1)
-    try:
-      while not rospy.is_shutdown():
-            r.sleep()
-    except KeyboardInterrupt:
-      print("Shutting down")
+    rospy.spin()
 if __name__ == '__main__':
     main(sys.argv)
